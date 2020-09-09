4b54318d5fe966a1a94c9c8b098599323c1829fd
Gargooie <ibanezed123@gmail.com>
2020-09-09 23:31:16 +0400

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import math
+
+array=[]
+
+def solution(area):
+    global array
+    array =[]
+    area_cut = area 
+    
+    def take_square(count):
+                
+        platform = int(math.sqrt(count))
+        platform = platform**2
+        array.append(platform)
+        count = count - platform
+        return count
+
+    while area_cut >0 :
+        area_cut = take_square(area_cut)
+
+    return(array)
+    
+
